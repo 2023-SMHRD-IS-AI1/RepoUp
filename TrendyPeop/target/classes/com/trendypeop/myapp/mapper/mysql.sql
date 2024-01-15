7ed1614bd20ebe5782caf10da198af62461462bf
smhrd <smhrd@DESKTOP-Q776JLA>
2024-01-15 09:04:10 +0900

    추가

--- TrendyPeop/target/classes/com/trendypeop/myapp/mapper/mysql.sql
+++ TrendyPeop/target/classes/com/trendypeop/myapp/mapper/mysql.sql
@@ -1,5 +1,11 @@
-
 select * from tb_user;
 
+alter table tb_closet add column style_idx int unsigned not null;
 
+alter table tb_closet add foreign key(style_idx) references tb_style(style_idx);
 
+alter table tb_closet drop column upload_img;
+
+alter table tb_closet change column uploaded_at added_at datetime default now();
+
+select * from tb_closet;
