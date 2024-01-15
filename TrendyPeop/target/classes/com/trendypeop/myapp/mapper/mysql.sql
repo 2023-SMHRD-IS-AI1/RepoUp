6290b9e58c45678c33bac4962d1dcea594ea4f0c
smhrd <smhrd@DESKTOP-Q776JLA>
2024-01-15 10:47:14 +0900

    커밋!

--- TrendyPeop/target/classes/com/trendypeop/myapp/mapper/mysql.sql
+++ TrendyPeop/target/classes/com/trendypeop/myapp/mapper/mysql.sql
@@ -9,3 +9,9 @@ alter table tb_closet drop column upload_img;
 alter table tb_closet change column uploaded_at added_at datetime default now();
 
 select * from tb_closet;
+
+alter table tb_user alter column user_type set default 'u';
+
+alter table tb_cody drop column cody_color;
+
+select * from tb_cody;
